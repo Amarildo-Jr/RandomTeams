c47f8ec8e3f31454b87d43f7c96dfef2c369d140
Amarildo Junior <ajunior283@outlook.com>
2022-03-05 22:21:30 -0300

    Finalizando a funcao para sortear os times

--- script.js
+++ script.js
@@ -10,16 +10,58 @@ jQuery(document).ready(function($){
     if( $(event.target).is('.popup-close') || $(event.target).is('.popup') ) {
       event.preventDefault();
       $(this).removeClass('is-visible');
+      const popup = document.getElementById("times-sorteados");
+      popup.innerHTML = ""
     }
   });
   //close popup when clicking the esc keyboard button
   $(document).keyup(function(event){
       if(event.which=='27'){
         $('.popup').removeClass('is-visible');
+        const popup = document.getElementById("times-sorteados");
+        popup.innerHTML = ""
       }
     });
 });
 
 function sortear() {
-    console.log("Faaaaaala")
+  let jogadores = document.getElementById("nomes").value.trim().split("\n");
+  jogadores = jogadores.sort(() => Math.random() - 0.5)
+  const quantidadeDeTimes = document.getElementById("ntimes").value;
+  console.log("Jogadores: " + jogadores)
+  console.log("Quantidade de times: " + quantidadeDeTimes)
+  const jogadoresPorTime = Math.floor(jogadores.length/quantidadeDeTimes);
+  console.log("Quantidade de jogadores por times: " + jogadoresPorTime)
+  let j = 1;
+  let times = []
+  for(i = 1; i <= quantidadeDeTimes; i++) {
+    console.log("Quantidade de jogadores " + jogadores.length)
+    let count = 0;
+    let time = []
+    while(j <= jogadoresPorTime * i){
+      time[count] = jogadores[j - 1]
+      j++;
+      count++;
+    }
+    times[i - 1] = time;
+    if(i == quantidadeDeTimes && j <= jogadores.length) {
+      console.log("i = " + i + " ; j = " + j)
+      let a = 0;
+      while(j <= jogadores.length){
+        if(a < quantidadeDeTimes){
+          times[a].push(jogadores[j - 1])
+          a++;
+          j++;
+        } else {
+          a = 0;
+        }
+      }
+    }
+  }
+  const popup = document.getElementById("times-sorteados");
+  for(i = 0; i < times.length; i++){
+    const codigo = `<div class='time-formado'><h2>Time` + (i+1) + `: </h2>` + times[i].join("<br>") + `</div>`
+    popup.innerHTML += codigo
+  }
+    
 }
